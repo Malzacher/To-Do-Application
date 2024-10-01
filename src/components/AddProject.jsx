bc460041b898302bd6210430e6e47236259d9f25
clearwater-agency <69623048+Malzacher@users.noreply.github.com>
2024-10-01 18:45:48 +1000

    Update AddProject.jsx

--- src/components/AddProject.jsx
+++ src/components/AddProject.jsx
@@ -1,23 +1,37 @@
 import Input from "./Input";
+import { useRef } from "react";
 
-export default function AddProject({onCancelProject}) {
+export default function AddProject({ onCancelProject }) {
+  let title = useRef();
+  let description = useRef();
+  let dueDate = useRef();
+
+  function manageAddProject() {
+    console.log(title.current.value);
+  }
 
   return (
     <div className="w-[35rem] mt-16  bg-white shadow-lg rounded-lg p-6">
       <menu className="flex items-center justify-end gap-4 mb-6">
         <li>
-          <button onClick={onCancelProject} className="text-gray-600 hover:text-gray-900 transition-colors duration-300">
+          <button
+            onClick={onCancelProject}
+            className="text-gray-600 hover:text-gray-900 transition-colors duration-300"
+          >
             Cancel
           </button>
         </li>
         <li>
-          <button onClick={null} className="px-6 py-2 bg-gray-800 text-white rounded hover:bg-gray-700 transition-colors duration-300">
+          <button
+            onClick={manageAddProject}
+            className="px-6 py-2 bg-gray-800 text-white rounded hover:bg-gray-700 transition-colors duration-300"
+          >
             Add
           </button>
         </li>
       </menu>
       <div className="space-y-4">
-        <Input label="Title" />
+        <Input ref={title} label="Title" />
         <Input label="Description" textarea />
         <Input label="Due Date" />
       </div>
